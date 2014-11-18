c5fbfc97d710d9cd4c0fdc854bfc77ec251698d9
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2014-11-18 17:45:42 +0800

    update readme

---
+++ examples/simple.js
@@ -0,0 +1,40 @@
+var PORT = 11211;
+var HOST = '127.0.0.1';
+var username = 'myname';
+var password = 'mypass';
+var memcached = require("../index");
+
+var client = memcached.createClient(PORT, HOST, {
+  username: username,
+  password: password
+});
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+// 10 为过期时间， 10秒
+client.set('hello', 'world', 10, function(err, res) {
+  console.log(err, res);
+});
+
+client.get('hello', 'world', function(err, res) {
+  console.log(err, res);
+});
+
+// 也可以不用设置过期时间
+client.set('number', 1, function(err, res) {
+  console.log(err, res);
+});
+
+client.increment('number', 2, function(err, res) {
+  console.log(err, res.toString());
+});
+
+client.decrement('number', 1, function(err, res) {
+  console.log(err, res.toString());
+});
+
+client.get('number', function(err, res) {
+  console.log(err, res);
+});
--- index.js
+++ index.js
@@ -356,7 +356,7 @@ MemcachedClient.prototype.on_data = function (data) {
 // put this try/catch in its own function because V8 doesn't optimize this well yet.
 function try_callback(client, callback, reply) {
   if (!reply || !reply.header || reply.header.status == undefined) {
-    client.emit("Internal server error", err);
+    client.emit("error", "can not pase message");
     return;
   }
 
@@ -377,6 +377,7 @@ function try_callback(client, callback, reply) {
     return;
   }
 
+  console.log(reply);
   callback(null, reply.val.toString());
 }
 
